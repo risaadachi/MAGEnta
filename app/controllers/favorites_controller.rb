340240307fb3f6199da30a9c061692f4dce88094
adachirisa <risa96ada@gmail.com>
2019-11-10 08:05:39 +0000

    [Finish]いいね機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,17 @@
 class FavoritesController < ApplicationController
+
+    def create
+    	code = Code.find(params[:code_id])
+    	favorite = current_user.favorites.new(code_id: code.id)
+    	favorite.save
+    	redirect_to code_path(code)
+    end
+
+    def destroy
+    	code = Code.find(params[:code_id])
+    	favorite = current_user.favorites.find_by(code_id: code.id)
+    	favorite.destroy
+    	redirect_to code_path(code)
+    end
+
 end
